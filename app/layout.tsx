7408affcbd10eac5f39924e5bcaf8c1730b8f339
suvaranjan <suvaranjan01@gmail.com>
2024-10-22 09:09:39 +0530

    changes css

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Figtree } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,7 +12,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-const pontano_sans = Figtree({
+const pontano_sans = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-pontano_sans',
@@ -64,7 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${pontano_sans.variable} scroll-smooth`}
+      className={`${pontano_sans.variable} scroll-smooth antialiased`}
       suppressHydrationWarning
     >
       <link
